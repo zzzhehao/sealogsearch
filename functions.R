0e69181c679f8529f777b3aaf09a069e0eaf0453
Zhehao Hu <hu_zhehao@hotmail.com>
2025-09-23 09:43:16 +0000

    merge init into functions

--- functions.R
+++ functions.R
@@ -1,9 +1,77 @@
-suppressMessages({
-    require(stringdist)
-    require(tidyverse)
-})
+init_cache <- function(search.field.custom = NULL) {
+    suppressMessages({
+        require(tidyverse)
+    })
+    # Run this function to process raw sealog export csv to cached RDS files
+    
+    dir.create("cache")
+    
+    cache.files <- list.files("cache", full.names = T)
+    sealog.files.path <- list.files("sealog", full.names = T, recursive = T)
+    
+    if (any(duplicated(sealog.files.path))) {
+        stop(paste0("Following files are duplicated: \n\n", paste(sealog.files.path[duplicated(sealog.files.path)], collapse = "\n"), collapse = ""))
+    }
+    
+    # Extract cruise/dive informations
+    
+    sealog.file.tbl <- sealog.files.path %>% str_split("/", simplify = T) %>% as_tibble()
+    cruises <- unique(sealog.file.tbl$V2) 
+    LUT.cruise_dive <- sealog.file.tbl$V3 %>% 
+        str_split("_", simplify = T) %>% 
+        as_tibble() %>% 
+        bind_cols(data.frame(path = sealog.files.path)) %>%
+        rename(cruise = V1, dive = V2) %>%
+        select(-V3) %>%
+        mutate(deployment = paste(cruise, dive, sep = "_"))
+    dives <- unique(LUT.cruise_dive$dive)
+    
+    # Construct cache files
+    
+    walk(1:nrow(LUT.cruise_dive), \(i) {
+        cache.path <- paste0("cache/", paste(LUT.cruise_dive$deployment[i], "default.RDS", sep = "_"), collapse = "")
+        if (cache.path %in% cache.files) {
+            cat(cache.path, "already cached.\n")
+            return()
+        }
+        
+        sealog.tbl <- read.table(LUT.cruise_dive$path[i], header = T, sep = ",")
+        sealog.tbl.4Search <- searchField.init(sealog.tbl, search.fields.user = search.field.custom)
+        
+        saveRDS(sealog.tbl.4Search, cache.path)
+        cat(cache.path, "successfully cached.\n")
+    })
+    
+    LUT.cruise_dive.2cache <- LUT.cruise_dive %>%
+        mutate(
+            cache_path = paste0("cache/", deployment, "_default.RDS")
+        )
+    
+    # Construct status report file
+    meta.replay <- youtube_yaml("replay-meta.yml") 
+    
+    dive.replayAvailable <- meta.replay %>% 
+        .$dive %>%
+        unique()
+    
+    LUT.cruise_dive.3replaycheck <- LUT.cruise_dive.2cache %>% 
+        mutate(replay = ifelse(dive %in% dive.replayAvailable, T, F))
+    
+    report <- list(
+        cruises = cruises,
+        dives = dives,
+        cruise_dive.tbl = LUT.cruise_dive.3replaycheck,
+        replayAvailable = dive.replayAvailable
+    )
+    
+    saveRDS(report, "cache/status_report.RDS")
+}
 
 searchField.init <- function(sealog.tbl, search.fields.user = NULL, search.options = F) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     sealog.fields <- colnames(sealog.tbl)
 
     if (search.options) {
@@ -41,6 +109,10 @@ searchField.init <- function(sealog.tbl, search.fields.user = NULL, search.optio
 }
 
 youtube_yaml <- function(path) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     youtube <- yaml::read_yaml(path) %>% bind_rows()
     
     youtube.cl <- youtube %>% 
@@ -59,6 +131,10 @@ youtube_yaml <- function(path) {
 }
 
 timecode2link <- function(UTCs, youtube.cl) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     if (any(!is.POSIXct(UTCs))) {
         UTCs <- as.POSIXct(UTCs, format = "%Y-%m-%d %H:%M", tz = "UTC")
     } 
@@ -83,6 +159,10 @@ timecode2link <- function(UTCs, youtube.cl) {
 }
 
 searchSealog <- function(searchTxt, searchRange.cruise="all", searchRange.dive="all", expansion = F, exact = F, ambiguity.threshold = 0.2, case_sensitive = F) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     if (searchTxt == "") {
         return(list(status="error", content="Keyword can't be empty"))
     }
@@ -157,16 +237,20 @@ searchSealog <- function(searchTxt, searchRange.cruise="all", searchRange.dive="
         )
     }) %>% unlist()
 
-    if (is.null(nrow(match.res))) {
+    sealog.match <- sealog.master[which(match.res),]
+    
+    if (nrow(sealog.match) == 0) {
         return(list(status="no_result", content="No result."))
     }
-    
-    sealog.match <- sealog.master[which(match.res),]
     
     return(list(status="exit", content=sealog.match))
 }
 
 searchres_youtube <- function(tbl, replay_meta_path) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     tbl.youtube <- bind_cols(
         tbl, 
         timecode2link(
@@ -178,6 +262,10 @@ searchres_youtube <- function(tbl, replay_meta_path) {
 }
 
 searchres_formatter <- function(tbl) {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
         tbl.format <- tbl %>%
             mutate(
                 Cruise = cruise_id,
@@ -197,6 +285,10 @@ searchres_formatter <- function(tbl) {
 }
 
 status_report <- function(path = "cache/status_report.RDS") {
+    suppressMessages({
+        require(stringdist)
+        require(tidyverse)
+    })
     status <- readRDS(path)
 
     list.cruise <- paste("## Available cruises", paste(paste("-", status$cruises, sep = " "), collapse = "\n"), sep = "\n\n")
